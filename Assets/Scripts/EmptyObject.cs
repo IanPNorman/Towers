8ee3675b3277d5491ff54df6fb2bb97394090450
mikephu <mikephu03@gmail.com>
2024-03-08 01:37:11 -0800

    feat: add test materials/prefabs

---
+++ Assets/Scripts/EmptyObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        /**
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject current = new GameObject();
+            current.AddComponent<BoxCollider>();
+        }
+        **/
+    }
+}
