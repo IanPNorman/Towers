225cf41b493dc13eec9732567d3fd8dd74641300
Owen <olovett214@gmail.com>
2024-03-22 10:41:01 -0700

    added quit function

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -9,4 +9,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("UITestScene");
     }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
 }
