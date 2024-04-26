f6a8204cabc54c9c9128b32af79831dc5473c3a6
IanPNorman <ianpk7211@gmail.com>
2024-04-26 11:44:56 -0700

    Demo Completed Kinda

--- Assets/TutorialInfo/Scripts/EnemyManager/Enemy.cs
+++ Assets/TutorialInfo/Scripts/EnemyManager/Enemy.cs
@@ -99,7 +99,7 @@ public class Enemy : MonoBehaviour
 
     public void MoveTowardsGoal()
     {
-        Quaternion rotation = Quaternion.LookRotation(target.transform.position - transform.position);
+        //Quaternion rotation = Quaternion.LookRotation(target.transform.position - transform.position);
         //transform.rotation = rotation;
         Vector3 direction = target.position - transform.position;
         transform.Translate(direction.normalized * (speed * Time.deltaTime));
